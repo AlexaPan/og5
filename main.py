ba5cdf0583202ca3cecbd5df906df4a9b0e28db7
AlexaPan <xvspan@gmail.com>
2025-01-16 14:12:36 +0300

    add const code

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+import random
+
 import pygame
 
 pygame.init()
 
-running = True
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+pygame.display.set_caption("Shooting Gallery Game")
+icon = pygame.image.load("Img/apple.jpg")
+pygame.display.set_icon(icon)
+
+target_image = pygame.image.load("Img/apple.jpg")
+target_width = 100
+target_height = 100
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+color = (0, 20, 0)
+
+running = True
 while running:
     pass
 
